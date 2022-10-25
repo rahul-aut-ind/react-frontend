bac3e3101cd694865c99866ab5f2c1b0ca7482b8
Rahul Upadhyay <rahulupadhyay@Gymondo-NP0Q4KJ2VD.fritz.box>
2022-10-25 08:28:17 +0200

    improving the look  of list of Workouts

--- src/Components/WorkoutList/Workouts.js
+++ src/Components/WorkoutList/Workouts.js
@@ -2,8 +2,9 @@ import React from 'react';
 import WorkoutDetails from "./WorkoutDetails";
 
 let style = {
-    padding: '0px 20px',
-    fontSize: 12
+    //padding: '0px 10px',
+    fontSize: 13,
+    width: 500
 }
 
 function Workouts(props) {
@@ -12,16 +13,16 @@ function Workouts(props) {
 
     return (
         <li className="list-group-item" style={{backgroundColor: props.children.isAvailable ? 'white' : 'lightgray'}}>
-            <div className="media align-items-lg-center flex-column flex-lg-row p-1 d-flex">
+            <div className="media align-items-lg-center flex-column flex-lg-row p-1">
                 <div className="media-body order-2 order-lg-1">
                     <h6 className="mt-0 font-weight-bold mb-2">{props.children.pName}</h6>
-                    <div className="d-flex align-items-center mt-1">
+                    <div className="d-flex align-items-center">
                         <div style={style}>Category: <b>{props.children.category}</b></div>
                         <div style={style}>StartDate : <b>{props.children.date}</b></div>
                         {props.children.isAvailable ?
                             <button onClick={props.showWorkoutDetails}
-                               value={props.children._id}
-                                className={badgeClass}>
+                                    value={props.children._id}
+                                    className={badgeClass}>
                                 Available to Enroll
                             </button>
                             :
