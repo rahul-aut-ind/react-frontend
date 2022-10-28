cd143db235190a8137eebb83bea0d3f4d47d895f
Rahul Upadhyay <rahulupadhyay@Gymondo-NP0Q4KJ2VD.fritz.box>
2022-10-28 09:35:31 +0200

    changing post method to get for workouts paginated response

--- backend-service/server.js
+++ backend-service/server.js
@@ -14,10 +14,6 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 app.use(cors())
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     next();
-// });
 
 // Configure Mongo
 const db = "mongodb://localhost/workouts";
@@ -59,15 +55,20 @@ app.get("/workouts/all", (req, res) => {
         });
 });
 
-app.post("/workouts", (req, res) => {
-
-    // get query params
-    let {pageNo = 1, itemsToDisplay = 20} = req.query;
-    if (pageNo < 1) pageNo = 1;
+app.get("/workouts", (req, res) => {
 
     const defaultFilter = {$ne: null};
-    const category = req.body.category === "" ? defaultFilter : req.body.category;
-    const date = req.body.startDate === "" ? defaultFilter : req.body.startDate;
+    // get query params
+    let {pageNo = 1, itemsToDisplay = 20, category = "", startDate = ""} = req.query;
+
+    if (pageNo < 1) pageNo = 1;
+
+    function isEmpty(value) {
+        return value === undefined || value === null || value === "\"\"" || value === "\'\'" || value.length === 0;
+    }
+
+    category = isEmpty(category) ? defaultFilter : category;
+    startDate = isEmpty(startDate) ? defaultFilter : startDate;
     const skip = (pageNo - 1) * itemsToDisplay;
 
     let ToDate = undefined;
@@ -75,12 +76,16 @@ app.post("/workouts", (req, res) => {
     let totalCount = undefined;
     let filterCriteria;
 
-    if (date != defaultFilter) {
-        ToDate = new Date(new Date(date).getFullYear(), new Date(date).getMonth() + 1).toISOString();
-        FromDate = new Date(new Date(date).getFullYear(), new Date(date).getMonth()).toISOString();
+    if (startDate != defaultFilter) {
+        try {
+            ToDate = new Date(new Date(startDate).getFullYear(), new Date(startDate).getMonth() + 1).toISOString();
+            FromDate = new Date(new Date(startDate).getFullYear(), new Date(startDate).getMonth()).toISOString();
 
-        filterCriteria = {category: category, startDate: {$gte: FromDate, $lte: ToDate}};
-        //console.log("Date Filtered > " + filterCriteria);
+            filterCriteria = {category: category, startDate: {$gte: FromDate, $lte: ToDate}};
+            //console.log("Date Filtered > " + filterCriteria);
+        } catch (err) {
+            return res.status(500).json({"msg": 'No Document in Database matching criteria..'});
+        }
     } else {
         filterCriteria = {category: category, startDate: defaultFilter};
         //console.log("All Dates > " + filterCriteria);
@@ -94,7 +99,7 @@ app.post("/workouts", (req, res) => {
             //console.log("Total Count > " + totalCount);
         }
         if (totalCount === 0) {
-            return res.status(500).json({"msg": 'No Document in Database..'});
+            return res.status(500).json({"msg": 'No Document in Database matching criteria..'});
         } else {
             //get paginated documents
             Workouts.find(filterCriteria).sort({_id: -1})
@@ -109,5 +114,4 @@ app.post("/workouts", (req, res) => {
             });
         }
     })
-
 })
